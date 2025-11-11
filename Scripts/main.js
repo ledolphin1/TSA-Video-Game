b2c3532ace7b6221dc27de08372a94025ebfc465
ledolphin1 <leoysscott@gmail.com>
2025-11-10 20:56:53 -0800

    Add files via upload

---
+++ Scripts/main.js
@@ -0,0 +1,23 @@
+// src/main.js
+import Phaser from 'phaser';
+import MainScene from './scenes/MainScene.js';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: '#2d2d2d',
+  parent: 'game-container',
+  scene: [MainScene],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
+  }
+};
+
+window.addEventListener('load', () => {
+  const game = new Phaser.Game(config);
+});
