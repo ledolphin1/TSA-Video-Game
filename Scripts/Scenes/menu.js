22e8c491ac8b58aef0fa34bc80a0f02950a04c68
kabirjoshi99 <kabirj.science@gmail.com>
2025-12-14 15:08:55 -0800

    added menu with play button and title. I played with colors a little, but we can change it. Will add assets as needed for aesthetics; it is currently just text.

---
+++ Scripts/Scenes/menu.js
@@ -0,0 +1,41 @@
+import Phaser from 'phaser';
+
+export default class Menu extends Phaser.Scene {
+  constructor() {
+    super({ key: 'Menu' });
+  }
+
+  create() {
+    const { width, height } = this.scale;
+
+    // Title text
+    this.add.text(width / 2, height / 2 - 70, 'MENU', {
+      fontSize: '48px',
+      color: '#ffffff'
+    }).setOrigin(0.5);
+
+    // Play button
+    const playButton = this.add.text(width / 2, height / 2, 'PLAY', {
+      fontSize: '24px',
+      color: '#cc0000ff',
+      backgroundColor: '#000000',
+      padding: { x: 12, y: 6 }
+    })
+    .setOrigin(0.5)
+    .setInteractive({ useHandCursor: true });
+
+    // Hover effects
+    playButton.on('pointerover', () => {
+      playButton.setStyle({ color: '#e78585ff' });
+    });
+
+    playButton.on('pointerout', () => {
+      playButton.setStyle({ color: '#cc0000ff' });
+    });
+
+    // Start the game
+    playButton.on('pointerdown', () => {
+      this.scene.start('MainScene');
+    });
+  }
+}
