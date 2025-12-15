4d08d5b7e0d133e745c95ddafbb1058404a591e0
vryCoolBoi <ilaxman.2022@gmail.com>
2025-12-14 22:31:55 -0800

    added intro.js file

---
+++ Scripts/Scenes/intro.js
@@ -0,0 +1,13 @@
+import Phaser from 'phaser';
+
+export default class Intro extends Phaser.Scene {
+  constructor() {
+    super({ key: 'Intro' });
+  }
+  preload(){
+    
+  }
+  create() {
+
+  }
+}
--- Scripts/main.js
+++ Scripts/main.js
@@ -2,17 +2,18 @@ import Phaser from 'phaser';
 import Menu from './Scenes/menu.js';
 import MainScene from './Scenes/mainscene.js';
 import Pause from "./Scenes/pause.js"
+import Intro from './Scenes/intro.js';
 
 const config = {
   type: Phaser.AUTO,
   width: 400,
   height: 224,
   zoom: 4.5,
-  backgroundColor: '#494949ff',
+  backgroundColor: '#ff0000ff',
   pixelArt: true,
   parent: 'game-container',
 
-  scene: [Menu, MainScene,Pause],
+  scene: [Intro, Menu, MainScene,Pause],
 
   physics: {
     default: 'arcade',
