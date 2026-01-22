cc91fa1ccaefe72fcadcc8a8e492f4998cc45210
ledolphin1 <leoysscott@gmail.com>
2026-01-22 00:13:40 -0800

    Fix scene name from 'Mainscene' to 'MainScene'

--- Scripts/Scenes/overworld.js
+++ Scripts/Scenes/overworld.js
@@ -201,7 +201,7 @@ export default class Overworld extends Phaser.Scene {
             if (Phaser.Input.Keyboard.JustDown(this.interactKey)) {
                 // Stop music before switching if needed
                 this.sound.stopAll();
-                this.scene.start("Mainscene");
+                this.scene.start("MainScene");
             }
         });
 
