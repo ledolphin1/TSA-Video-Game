01a43b0aa15ced564d90a4529ae2fc9a91a1f31d
ledolphin1 <leoysscott@gmail.com>
2026-01-21 22:51:36 -0800

    made phaser importmap work

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,4 +1,4 @@
-import Phaser from 'phaser';
+import { Phaser } from 'phaser';
 import Menu from './Scenes/menu.js';
 import MainScene from './Scenes/mainscene.js';
 import Pause from "./Scenes/pause.js"
