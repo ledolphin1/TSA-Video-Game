941f01ae1da98ea189253d1524d746b0f93129e9
vryCoolBoi <ilaxman.2022@gmail.com>
2026-03-12 20:15:39 -0700

    Revert "Update boss.js"
    This reverts commit 30c930caf6d55d7c51f05c325cfb40305a4fa36f.

--- Scripts/Scenes/boss.js
+++ Scripts/Scenes/boss.js
@@ -387,8 +387,7 @@ export default class BossScene extends Phaser.Scene {
     // Reset Player Position and Physics
     this.playerVisual.clearTint();
     this.playerVisual.setTexture("player_still"); // Reset animation to idle
-    console.log(player.x,player.y)
-    this.player.enableBody(true, 79, 20, true, false); // Reset to start pos, keep hidden
+    this.player.enableBody(true, 79, 232, true, false); // Reset to start pos, keep hidden
     this.playerVisual.setAlpha(1);
     this.player.setVelocity(0, 0);
     this.lastFiredTime = 0;
