194852780a2173a9259d295a7b2de22caf627409
vryCoolBoi <ilaxman.2022@gmail.com>
2026-03-15 16:50:47 -0700

    wave commit

---
+++ Scripts/Scenes/Functions/wave.js
@@ -0,0 +1,43 @@
+  const waveProj = function (time) {
+    this.projectileOnCooldown = true;
+    this.projectileCooldownStart = time;
+    this.waveAttackDmg = 2;
+    this.lastFiredTime = time;
+    const proj = this.physics.add.sprite(this.player.x,this.player.y,"wave").setData("enemies_pierced",0);
+    proj.flipX = this.player.flipX ? true : false;
+    this.physics.add.existing(proj);
+    this.playerProjectiles.add(proj); // Use separate group!
+    proj.enemiesColided = [];
+
+    const velocity = this.player.flipX ? -400 : 400;
+    proj.body.allowGravity = false;
+    proj.body.setVelocityX(velocity);
+    proj.spawnimmunity = 2;
+    const opacityInterval = setInterval(function(){
+        if (proj.spawnimmunity){
+            proj.spawnimmunity--;
+            return;
+        }
+        proj.setAlpha(proj.alpha - 0.25)
+        if (proj.alpha == 0){
+            proj.destroy();
+            clearInterval(opacityInterval);
+            console.log("i shouldn't come more than once")
+            return;
+        }
+    }.bind(this),50)
+    // Add collision with enemies
+    this.physics.add.overlap(proj, this.enemies, (projectile, enemy) => {
+        if(projectile.enemiesColided.includes(enemy)){
+            return
+        }
+        projectile.enemiesColided.push(enemy);
+        console.log("this happened")
+        this.projectileEnemyCollisionHandle(projectile,enemy,this.waveAttackDmg)
+    });
+    // hit wall
+    this.physics.add.collider(proj, this.ground, () => {
+      proj.destroy();
+    });
+  }
+  export default waveProj;
